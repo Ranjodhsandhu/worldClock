cf7fc502819ee53e6f5ece59719680606f83f29b
Ranjodh Singh <ranjodhsingh24@gmail.com>
2020-04-29 23:35:10 -0400

    Updates

---
+++ precache-manifest.5366326267a41eaf64e9685032a9a4c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1e309a28efa5874d9116c36e250924",
+    "url": "/worldClock/index.html"
+  },
+  {
+    "revision": "98f4960cf388f2341cca",
+    "url": "/worldClock/static/css/main.a48feb7d.chunk.css"
+  },
+  {
+    "revision": "4272369a4d7e0a5cdab9",
+    "url": "/worldClock/static/js/2.115be65b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/worldClock/static/js/2.115be65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f4960cf388f2341cca",
+    "url": "/worldClock/static/js/main.aff5d96b.chunk.js"
+  },
+  {
+    "revision": "85f121cb89bc3637b252",
+    "url": "/worldClock/static/js/runtime-main.d41d9c25.js"
+  }
+]);
